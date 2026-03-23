7e924357463e67048957b5ab463d9d7b62e01591
Mitra <120065660+Mitra-88@users.noreply.github.com>
2026-03-23 16:38:02 +0100

    Improve site-wide page design

--- src/static/js/fonts.js
+++ src/static/js/fonts.js
@@ -1,37 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const fontSelect = document.getElementById('font');
-    const colorSelect = document.getElementById('color');
+    const fontSelect = document.getElementById("font");
+    const colorSelect = document.getElementById("color");
 
     const colorOptionsMap = new Map([
-        ['Blue', 'Blue'],
-        ['Orange', 'Orange'],
-        ['Gold', 'Gold'],
-        ['Yellow', 'Yellow']
+        ["Blue", "Blue"],
+        ["Orange", "Orange"],
+        ["Gold", "Gold"],
+        ["Yellow", "Yellow"]
     ]);
 
     const colorMap = new Map([
-        ['1', new Set(['Blue', 'Orange', 'Gold'])],
-        ['2', new Set(['Blue', 'Orange', 'Gold'])],
-        ['3', new Set(['Blue', 'Orange'])],
-        ['4', new Set(['Blue', 'Orange', 'Yellow'])],
-        ['5', new Set(['Orange'])]
+        ["1", new Set(["Blue", "Orange", "Gold"])],
+        ["2", new Set(["Blue", "Orange", "Gold"])],
+        ["3", new Set(["Blue", "Orange"])],
+        ["4", new Set(["Blue", "Orange", "Yellow"])],
+        ["5", new Set(["Orange"])]
     ]);
 
-    if (fontSelect) {
-        fontSelect.addEventListener('change', updateColorOptions);
-        updateColorOptions();
+    if (!fontSelect || !colorSelect) {
+        return;
     }
+
+    fontSelect.value = fontSelect.dataset.selected || fontSelect.value;
+    updateColorOptions();
+    fontSelect.addEventListener("change", updateColorOptions);
 
     function updateColorOptions() {
         const fontValue = fontSelect.value;
         const colors = colorMap.get(fontValue) || new Set();
-        const colorOptions = [...colors].map(color => {
+        const previousSelection = colorSelect.dataset.selected || colorSelect.value;
+        const colorOptions = [...colors].map((color) => {
             const value = color.toLowerCase();
             const text = colorOptionsMap.get(color);
-            return `<option value="${value}">${text}</option>`;
-        }).join('');
+            const selected = previousSelection === value ? " selected" : "";
+            return `<option value="${value}"${selected}>${text}</option>`;
+        }).join("");
 
         colorSelect.innerHTML = colorOptions;
+        colorSelect.dataset.selected = colorSelect.value;
     }
 });
 
